62843ac89086d5c0ef7c89ae3e98cfc7ada00afc
Tyler Johnson <yonstib@gmail.com>
2016-08-22 07:24:28 -0600

    improved toolbox functionality

--- app/src/main/java/com/example/tyler/lockpick/MainActivity.java
+++ app/src/main/java/com/example/tyler/lockpick/MainActivity.java
@@ -1,25 +1,34 @@
 package com.example.tyler.lockpick;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final boolean TOOLBOX_CLOSED = false;
+    private static final boolean TOOLBOX_OPEN = true;
+    private boolean toolbox_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        toolbox_state = TOOLBOX_CLOSED;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                }
-            });
-        }
     }
 
+    /**
+     * When the toolbox button is clicked
+     * @param view
+     */
+    public void toolboxTap(View view) {
+        if (toolbox_state == TOOLBOX_CLOSED){
+            Toast.makeText(this,"Opening the toolbox", Toast.LENGTH_SHORT).show();
+            toolbox_state = TOOLBOX_OPEN;
+        } else {
+            Toast.makeText(this,"Closing the toolbox", Toast.LENGTH_SHORT).show();
+            toolbox_state = TOOLBOX_CLOSED;
+        }
+    }
 }
