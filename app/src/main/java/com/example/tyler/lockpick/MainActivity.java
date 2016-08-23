c7fe9009f4924a5b709ef02123351df642218594
Tyler Johnson <yonstib@gmail.com>
2016-08-22 23:44:12 -0600

    Data is obtained from gyroscope

--- app/src/main/java/com/example/tyler/lockpick/MainActivity.java
+++ app/src/main/java/com/example/tyler/lockpick/MainActivity.java
@@ -1,29 +1,49 @@
 package com.example.tyler.lockpick;
 
-import android.graphics.PointF;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.renderscript.Type;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import static java.lang.Math.abs;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
     private static final boolean TOOLBOX_CLOSED = false;
     private static final boolean TOOLBOX_OPEN = true;
+    private SensorManager mSensorManager;
+    private lockRotation rotationSensor;
     private boolean toolbox_state;
-    PointF start = new PointF();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         toolbox_state = TOOLBOX_CLOSED;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lockpick movement
         findViewById(R.id.lockpick).setOnTouchListener(new View.OnTouchListener(){
             float x_offset = 0;
             float y_offset = 0;
@@ -49,6 +69,62 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // Start Sensor Manager
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        rotationSensor = new lockRotation();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        rotationSensor.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        rotationSensor.stop();
+    }
+
+    class lockRotation implements SensorEventListener {
+        private Sensor mRotationVectorSensor;
+        private final float[] mRotationMatrix = new float[9];
+
+        public lockRotation(){
+            mRotationVectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+            // initialize the rotation matrix to identity
+            mRotationMatrix[ 0] = 1;
+            mRotationMatrix[ 3] = 1;
+            mRotationMatrix[ 6] = 1;
+        }
+
+        public void start(){
+            // Enable the sensor when the activity is resumed, ask for 10 ms updates
+            mSensorManager.registerListener(this,mRotationVectorSensor,10000);
+        }
+
+        public void stop() {
+            mSensorManager.unregisterListener(this);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
+                SensorManager.getRotationMatrixFromVector(mRotationMatrix,event.values);
+                //String matrix = "";
+                //for (float i:mRotationMatrix){
+                //    matrix += i + " ";
+                //}
+                //System.out.println(matrix);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
     }
 
     /**
@@ -64,4 +140,16 @@ public class MainActivity extends AppCompatActivity {
             toolbox_state = TOOLBOX_CLOSED;
         }
     }
+
+    /**
+     * Close the toolbox if the background is tapped
+     * @param view
+     */
+    public void backgroundTap(View view) {
+        if (toolbox_state){
+            Toast.makeText(this,"Closing the toolbox", Toast.LENGTH_SHORT).show();
+            toolbox_state = TOOLBOX_CLOSED;
+        }
+    }
 }
+
